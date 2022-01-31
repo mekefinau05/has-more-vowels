79b4e06d899da90d34e449e98c7155b3a1558924
Meke Finau <lonifinau.09@gmail.com>
2022-01-31 09:46:38 -0700

    hasMoreVowels

---
+++ hasMoreVowels.js
@@ -0,0 +1,25 @@
+const hasMoreVowels = word => {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelCount = 0
+    let vowelsNeeded = word.length / 2
+    let wordSplit = word.toLowerCase().split('')
+  
+    for (let i = 0; i < wordSplit.length; i++) {
+      let letter = wordSplit[i]
+  
+      if (vowels.includes(letter)) {
+        vowelCount++
+      }
+  
+      if (vowelCount > vowelsNeeded) {
+        return true
+      }
+    }
+  
+    return false
+  }
+  
+//   console.log(hasMoreVowels('moose'))
+//   console.log(hasMoreVowels('Aal'))
+//   console.log(hasMoreVowels('yay'))
+//   console.log(hasMoreVowels('mice'))
